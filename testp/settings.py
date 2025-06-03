8d1c846ad321a324ddc678c20cf778d07de26f3f
Bassem_Oueslati <104148399+Bassemoueslati@users.noreply.github.com>
2025-06-03 11:38:48 +0000

    5_commit

--- testp/settings.py
+++ testp/settings.py
@@ -203,7 +203,9 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     
 ]
-CORSLLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
